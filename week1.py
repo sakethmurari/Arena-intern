3b702a6a9af479a478384a2dc6ea1422a542ea0f
Murari Saketh Datta <101931291+sakethmurari@users.noreply.github.com>
2025-11-14 21:57:22 +0530

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,7 @@
+# Personal Introduction Program
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+hobby = input("Enter your favorite hobby: ")
+
+print(f"Hello {name} You are {age} years old and love {hobby}. Nice to meet you!")
